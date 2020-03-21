b492a8a72e758efd9c17b9278d7ebd4000089a10
KJ <cozykoji18@gmail.com>
2020-03-22 01:27:02 +0900

    App.js edit

--- App.js
+++ App.js
@@ -1,67 +1,47 @@
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { SplashScreen } from 'expo';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset'
+import { Font } from 'expo-font'
+import fonts from 'app/src/fonts';
+import images from 'app/src/images';
 
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import useLinking from './navigation/useLinking';
+export default class App extends React.Component {
+  static defaultProps = {
+    skipLoadingScreen: false
+  }
 
-const Stack = createStackNavigator();
-
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
-
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
-
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
-
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoadingComplete: false
     }
+  }
 
-    loadResourcesAndDataAsync();
-  }, []);
+  loadResourcesAsync = async () => {
+    await Asset.loadAsync(Object.keys(images).map( key => images[key] ));
+    await Font.loadAsync(fonts);
+    return true;
+  }
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
+  render() {
+    const { isLoadingComplete } = this.state;
+    const { skipLoadingScreen } = this.props;
+
+    if (!isLoadingComplete && !skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.loadResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      );
+    }
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Hello World</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
---
+++ src/images.js
@@ -0,0 +1,6 @@
+const icon = require('app/assets/images/icon.png');
+const images = {
+  icon,
+}
+
+export default images;
